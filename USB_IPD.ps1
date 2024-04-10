288b83d0beeba0a451b23fc735aed46187f2ce5d
maitrepathelin <maitrepathelin@hotmail.fr>
2024-04-10 15:24:08 +0200

    Create USB_IPD.ps1

---
+++ USB_IPD.ps1
@@ -0,0 +1,14 @@
+# Script PowerShell pour attacher un dispositif USB et modifier les permissions sur /dev/ttyUSB0 dans WSL
+start-sleep -s 60
+# Variable pour stocker le mot de passe de l'utilisateur WSL
+
+
+# Attacher le dispositif USB au WSL
+usbipd attach --wsl --busid 4-2
+start-sleep -s 5
+usbipd attach --wsl --busid 4-2
+start-sleep -s 5
+usbipd attach --wsl --busid 4-2
+start-sleep -s 5
+
+Je répète ici plusieurs fois la tâche, car pour une raison obscure ca échoue souvent la première fois, mais réussi après. Ca mange pas de pain de bourrer le prompt.
