8c698afaa5dd3b0c7be3d33b768e236af8983709
maitrepathelin <maitrepathelin@hotmail.fr>
2024-04-15 13:48:36 +0200

    Add files via upload

---
+++ decorecoUSB.ps1
@@ -0,0 +1,7 @@
+﻿Write-Host "Début script USBIPD"
+usbipd.exe detach -b 3-1
+Write-Host "Périphérique détaché, attente 10 secondes"
+Start-sleep -Seconds 10
+usbipd.exe attach -a -b 3-1 -w
+Write-Host "Périphérique rattaché, attente 10 seconde et fermeture"
+Start-Sleep -Seconds 10
